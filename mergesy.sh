3c0e306d91274ae8aca3e2e8884829992409dc4f
sbq88 <sbq88@126.com>
2025-02-10 10:21:06 +0800

    Update IP list 2025-02-10 10:21:05

---
+++ mergesy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 下载文件并添加标签
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/HK.txt | sed 's/$/&#HK/' >hk.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/JP.txt | head -n 10 | sed 's/$/&#JP/' >jp.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/KR.txt | head -n 10 | sed 's/$/&#KR/' >kr.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/SG.txt | head -n 10 | sed 's/$/&#SG/' >sg.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/US.txt | head -n 10 | sed 's/$/&#US/' >us.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/GB.txt | head -n 8 | sed 's/$/&#GB/' >gb.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/AU.txt | head -n 4 | sed 's/$/&#AU/' >au.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/DE.txt | head -n 8 | sed 's/$/&#DE/' >de.txt
+curl -s https://gh.llkk.cc/https://raw.githubusercontent.com/hhyyx1/cfip/refs/heads/main/ip443/NL.txt | head -n 8 | sed 's/$/&#NL/' >nl.txt
+
+# 合并文件
+cat hk.txt jp.txt kr.txt sg.txt us.txt gb.txt au.txt de.txt nl.txt>ipsy.txt
+
+# 清理临时文件
+rm hk.txt jp.txt kr.txt sg.txt us.txt gb.txt au.txt de.txt nl.txt
+
+# 提交到git
+git add ipsy.txt
+git commit -m "Update IP list $(date +'%Y-%m-%d %H:%M:%S')"
+git push
